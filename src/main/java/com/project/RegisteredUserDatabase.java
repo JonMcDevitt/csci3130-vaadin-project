0defad8dbc32e9d822fa86533111367eed058a3c
Jilili <dindanmao@hotmail.com>
2017-02-23 12:53:24 -0400

    created RegisteredUser class for storing registered user info created RegisteredUserDatabase class for storing Registered User objects

---
+++ src/main/java/com/project/RegisteredUserDatabase.java
@@ -0,0 +1,53 @@
+package com.project;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.ArrayList;
+
+/** Separate Java service class.
+ * Backend implementation for the address book application, with "detached entities"
+ * simulating real world DAO. Typically these something that the Java EE
+ * or Spring backend services provide.
+ */
+// Backend service class. This is just a typical Java backend implementation
+// class and nothing Vaadin specific.
+public class RegisteredUserDatabase {
+
+    private static RegisteredUserDatabase instance;
+
+    private ArrayList<RegisteredUser> userDatabase;
+    
+    
+    public RegisteredUserDatabase() {
+    	userDatabase = new ArrayList<>();
+    	RegisteredUser testUser = new RegisteredUser("test@test.com", "test@test.com", "p4ssw0rd", "p4ssw0rd", "test", "test", "test");
+    	userDatabase.add(testUser);
+    }
+    
+    public ArrayList<RegisteredUser> getDatabase(){
+    	return userDatabase;
+    }
+
+    public synchronized long count() {
+        return userDatabase.size();
+    }
+    
+    
+    public boolean delete(RegisteredUser user) {
+        for (RegisteredUser r : userDatabase){
+        	if(r.getUserName().equals(user.getUserName())){
+        		userDatabase.remove(r);
+        		return true;
+        	}
+        }
+        return false;
+    }
+
+    public synchronized void save(RegisteredUser user) {
+        userDatabase.add(user);
+    }
+}
+
