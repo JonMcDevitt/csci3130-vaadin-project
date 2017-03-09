f35ad628fef0c1480908aabb0c2f5354a01c60c2
Nick Broderick <nsbroderick@outlook.com>
2017-03-09 13:41:16 -0400

    Intermediate commit to save work

---
+++ src/main/java/com/project/BarcodeScanner.java
@@ -0,0 +1,87 @@
+package com.project;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+public class BarcodeScanner extends CustomComponent {
+    
+    private static final long serialVersionUID = -2387815294780431466L;
+    
+    private TextField hiddenTextField;
+    
+    private Button activateButton;
+    private Button deactivateButton;
+    
+    private VerticalLayout verticalLayout;
+    private HorizontalLayout horizontalLayout;
+    
+    private Optional<Consumer<String>> callbackOptional;
+
+    public BarcodeScanner() {
+        hiddenTextField = new TextField();
+        activateButton = new Button();
+        deactivateButton = new Button();
+        
+        setupHiddenTextField(hiddenTextField);
+        setupActivateButton(activateButton);
+        setupDeactivateButton(deactivateButton);
+        
+        horizontalLayout = new HorizontalLayout();
+        horizontalLayout.addComponents(activateButton, deactivateButton);
+        
+        verticalLayout = new VerticalLayout();
+        verticalLayout.addComponents(hiddenTextField, horizontalLayout);
+        
+        setCompositionRoot(verticalLayout);
+    }
+
+    private void setupHiddenTextField(TextField textField) {
+        textField.setVisible(true);
+        textField.setImmediate(true);
+        textField.setWidth("25%");
+        
+        textField.addBlurListener(e -> {
+            if (!activateButton.isEnabled() && deactivateButton.isEnabled()) {
+                deactivateButton.setEnabled(false);
+                activateButton.setEnabled(true);
+            }
+        });
+    }
+    
+    private void setupActivateButton(Button activateButton) {
+        activateButton.setCaption("Start scanning");
+        activateButton.setEnabled(true);
+        activateButton.setDisableOnClick(true);
+        activateButton.setWidth("12.5%");
+        
+        activateButton.addClickListener(e -> {
+            deactivateButton.setEnabled(true);
+            hiddenTextField.focus();
+        });
+    }
+    
+    private void setupDeactivateButton(Button deactivateButton) {
+        deactivateButton.setCaption("Stop scanning");
+        deactivateButton.setEnabled(false);
+        deactivateButton.setDisableOnClick(true);
+        deactivateButton.setWidth("12.5%");
+        
+        deactivateButton.addClickListener(e -> {
+            activateButton.setEnabled(true);
+        });
+    }
+    
+    private void toggleButtons() {
+        Button enabledButton = activateButton.isEnabled() ? activateButton : deactivateButton;
+        Button disabledButton = activateButton.isEnabled() ? deactivateButton : activateButton;
+        enabledButton.setEnabled(false);
+        disabledButton.setEnabled(true);
+    }
+
+}
