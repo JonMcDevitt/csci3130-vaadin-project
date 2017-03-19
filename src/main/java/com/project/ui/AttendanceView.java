7a3b292a6016bb8546fa7dbf06d5b8cf4323d447
Nick Broderick <nsbroderick@outlook.com>
2017-03-19 16:00:48 -0300

    Refactoring pass on AttendanceView, ClassDay

--- src/main/java/com/project/ui/AttendanceView.java
+++ src/main/java/com/project/ui/AttendanceView.java
@@ -18,125 +18,148 @@ import com.vaadin.ui.VerticalLayout;
 
 public class AttendanceView extends CustomComponent implements View {
     private static final long serialVersionUID = 8909424502341897571L;
-    
+
+    private Course course;
     private Grid attendanceGrid;
-	private ClassDay todayClassDay;
-	private BarcodeScannerComponent barcodeScannerComponent;
-	private IndexedContainer attendanceRecords;
-	private Button toCourseView;
-	private Label label;
-	
+    private ClassDay todayClassDay;
+    private BarcodeScannerComponent barcodeScannerComponent;
+    private IndexedContainer attendanceRecords;
+    private Button toCourseViewButton;
+    private Label label;
+
     private static final String FIRST_NAME = "First Name";
     private static final String LAST_NAME = "Last Name";
     private static final String BANNER_NUMBER = "Banner Number";
     private static final String BARCODE = "Barcode";
     private static final String PRESENT = "Present";
-	
-	public AttendanceView(Course course) {
-	    barcodeScannerComponent = new BarcodeScannerComponent();
-		attendanceGrid = new Grid();
+
+    public AttendanceView(Course course) {
+        this.course = course;
+
+        attendanceGrid = new Grid();
         todayClassDay = getTodayClassDay(course);
-		attendanceRecords = getAttendanceRecords(todayClassDay);
-		
-		configureGrid(todayClassDay);
-		configureLabel(course);
-		VerticalLayout layout = new VerticalLayout();
-		layout.setMargin(true);
-		configureBackButton(course);
-		layout.addComponents(label, attendanceGrid,barcodeScannerComponent, toCourseView);
-		layout.setSizeFull();
-		layout.setSpacing(true);
-		layout.setComponentAlignment(label,Alignment.MIDDLE_CENTER);
-		layout.setComponentAlignment(attendanceGrid,Alignment.MIDDLE_CENTER);
-		layout.setComponentAlignment(barcodeScannerComponent,Alignment.MIDDLE_CENTER);
-		layout.setComponentAlignment(toCourseView,Alignment.MIDDLE_CENTER);
-		
-		attendanceGrid.setContainerDataSource(attendanceRecords);
+        barcodeScannerComponent = new BarcodeScannerComponent();
+        attendanceRecords = getAttendanceRecords(todayClassDay);
+        label = new Label();
+        toCourseViewButton = new Button();
 
-		barcodeScannerComponent.onBarcodeScanned(s -> {
-		    todayClassDay.studentScanned(s);
-		    updateAttendanceGrid(s,PRESENT);
-		});
-		setCompositionRoot(layout);
-	}
-	
-	private void configureGrid(ClassDay classDay){
-		attendanceGrid.setContainerDataSource(attendanceRecords);
-		attendanceGrid.setWidth("100%");
-	}
-	
-	private void configureBackButton(Course course){
-		toCourseView = new Button("Back to Course View");
-		toCourseView.addClickListener(e ->{
-    		getUI().setContent(new CourseView(course));
-		});
-	}
-	
-	private void configureLabel(Course course){
-		label = new Label("Attendance for: " +course.getCourseCode()+" "+course.getCourseName());
-	}
+        configureGrid(todayClassDay);
+        configureLabel(label);
+        configureBackButton(toCourseViewButton);
 
-	@SuppressWarnings("unchecked")
-    private void updateAttendanceGrid(String barcode, String present) {
-	    Item record = attendanceRecords.getItem(barcode);
-	    if (record != null) {
-	        record.getItemProperty(PRESENT).setValue(present);
-	    }
+        attendanceGrid.setContainerDataSource(attendanceRecords);
+
+        barcodeScannerComponent.onBarcodeScanned(s -> {
+            todayClassDay.studentScanned(s);
+            updateAttendanceGrid(s, AttendanceStatus.PRESENT);
+        });
+
+        VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+
+        layout.addComponents(label, attendanceGrid, barcodeScannerComponent, toCourseViewButton);
+        layout.setSizeFull();
+        layout.setSpacing(true);
+        layout.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
+        layout.setComponentAlignment(attendanceGrid, Alignment.MIDDLE_CENTER);
+        layout.setComponentAlignment(barcodeScannerComponent, Alignment.MIDDLE_CENTER);
+        layout.setComponentAlignment(toCourseViewButton, Alignment.MIDDLE_CENTER);
+
+        setCompositionRoot(layout);
     }
-	
-	@SuppressWarnings("unchecked")
-    private static IndexedContainer getAttendanceRecords(ClassDay classDay) {
-		IndexedContainer attendanceRecords = new IndexedContainer();
-		
-		attendanceRecords.addContainerProperty(FIRST_NAME, String.class, "");
-		attendanceRecords.addContainerProperty(LAST_NAME, String.class, "");
-		attendanceRecords.addContainerProperty(BANNER_NUMBER, String.class, "");
-		attendanceRecords.addContainerProperty(BARCODE, String.class, "");
-		attendanceRecords.addContainerProperty(PRESENT, String.class, "Absent");
-		
-		if (classDay != null) {
-			for (Student s : classDay.getAbsentStudents()) {
-				Item item = attendanceRecords.addItem(s.getBarcode());
-				item.getItemProperty(FIRST_NAME).setValue(s.getFirstName());
-				item.getItemProperty(LAST_NAME).setValue(s.getLastName());
-				item.getItemProperty(BANNER_NUMBER).setValue(s.getId());
-				item.getItemProperty(BARCODE).setValue(s.getBarcode());
-				item.getItemProperty(PRESENT).setValue("Absent");
-			}
-			for (Student s : classDay.getAttendingStudents()) {
-				Item item = attendanceRecords.addItem(s.getBarcode());
-				item.getItemProperty(FIRST_NAME).setValue(s.getFirstName());
-				item.getItemProperty(LAST_NAME).setValue(s.getLastName());
-				item.getItemProperty(BANNER_NUMBER).setValue(s.getId());
-				item.getItemProperty(BARCODE).setValue(s.getBarcode());
-				item.getItemProperty(PRESENT).setValue("Present");
-			}
-		}
-		
-		return attendanceRecords;
-	}
 
-	@Override
-	public void enter(ViewChangeEvent event) {
-		
-	}
-	
-	private static boolean isToday(Date compareDate){
-		Date date = new Date();
-		if(compareDate.getYear()==date.getYear()&&compareDate.getDate()==date.getDate()
-				&&date.getMonth()==compareDate.getMonth()){
-			return true;
-		}
-		return false;
-			
-	}
-	
-	private ClassDay getTodayClassDay(Course course) {
-	    for (ClassDay c : course.getClassDays()) {
-            if (isToday(c.getStartTime())){
+    private void configureGrid(ClassDay classDay) {
+        attendanceGrid.setContainerDataSource(attendanceRecords);
+        attendanceGrid.setWidth("100%");
+    }
+
+    private void configureBackButton(Button backButton) {
+        toCourseViewButton.setCaption("Back to Course View");
+        toCourseViewButton.addClickListener(e -> {
+            getUI().setContent(new CourseView(course));
+        });
+    }
+
+    private void configureLabel(Label label) {
+        String courseCode = course.getCourseCode();
+        String courseName = course.getCourseName();
+        String caption = String.format("Attendance for: %s %s", courseCode, courseName);
+        label.setCaption(caption);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void updateAttendanceGrid(String barcode, AttendanceStatus status) {
+        Item record = attendanceRecords.getItem(barcode);
+        if (record != null) {
+            record.getItemProperty(PRESENT).setValue(status);
+        }
+    }
+
+    private static IndexedContainer getAttendanceRecords(ClassDay classDay) {
+        IndexedContainer attendanceRecords = new IndexedContainer();
+
+        attendanceRecords.addContainerProperty(FIRST_NAME, String.class, "");
+        attendanceRecords.addContainerProperty(LAST_NAME, String.class, "");
+        attendanceRecords.addContainerProperty(BANNER_NUMBER, String.class, "");
+        attendanceRecords.addContainerProperty(BARCODE, String.class, "");
+        attendanceRecords.addContainerProperty(PRESENT, AttendanceStatus.class, AttendanceStatus.ABSENT);
+
+        if (classDay != null) {
+            for (Student student : classDay.getAbsentStudents()) {
+                Item item = attendanceRecords.addItem(student.getBarcode());
+                setRecordItemProperties(item, student, AttendanceStatus.ABSENT);
+            }
+            for (Student student : classDay.getAttendingStudents()) {
+                Item item = attendanceRecords.addItem(student.getBarcode());
+                setRecordItemProperties(item, student, AttendanceStatus.PRESENT);
+            }
+        }
+
+        return attendanceRecords;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void setRecordItemProperties(Item item, Student student, AttendanceStatus status) {
+        item.getItemProperty(FIRST_NAME).setValue(student.getFirstName());
+        item.getItemProperty(LAST_NAME).setValue(student.getLastName());
+        item.getItemProperty(BANNER_NUMBER).setValue(student.getId());
+        item.getItemProperty(BARCODE).setValue(student.getBarcode());
+        item.getItemProperty(PRESENT).setValue(status);
+    }
+
+    @Override
+    public void enter(ViewChangeEvent event) {
+
+    }
+
+    private static boolean isToday(Date compareDate) {
+        Date date = new Date();
+        if (compareDate.getYear() == date.getYear() && compareDate.getDate() == date.getDate()
+                && date.getMonth() == compareDate.getMonth()) {
+            return true;
+        }
+        return false;
+
+    }
+
+    private ClassDay getTodayClassDay(Course course) {
+        for (ClassDay c : course.getClassDays()) {
+            if (isToday(c.getStartTime())) {
                 return c;
             }
         }
-	    return new ClassDay(new Date(), new Date(), course.getStudentRoster());
-	}	
+        
+        ClassDay classDay = new ClassDay(new Date(), new Date(), course.getStudentRoster());
+        course.getClassDays().add(classDay);
+
+        return classDay;
+    }
+
+    private enum AttendanceStatus {
+        PRESENT, ABSENT;
+
+        public String toString() {
+            return name();
+        }
+    }
 }
