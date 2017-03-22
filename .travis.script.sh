417ed0009db75a22b71a2993ce2b91b1bf44e67f
Jonathan <jon_mcdevitt@hotmail.com>
2017-03-22 17:25:03 -0300

    Setting up PhantomJS Tests
    Authors: Jonathan McDevitt
    
    Got builds working for tests. Now testing to see if this works on Travis.

--- .travis.script.sh
+++ .travis.script.sh
@@ -1,3 +1,3 @@
 	# Secure vars available, needed for TestBench tests
 	# Pull request inside repository or branch build
-	mvn -B -e -V -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE verify
+	mvn -B -e -V -Dvaadin.testbench.developer.license=487b5b4c-ff55-48cb-8d11-7c04b96a78cf verify
