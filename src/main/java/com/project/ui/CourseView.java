90ac45679b88972df1db36417d9ab921e3754bdc
Liam Gowan <liam@dal.ca>
2017-04-09 17:27:22 -0300

    Attempted change to title

--- src/main/java/com/project/ui/CourseView.java
+++ src/main/java/com/project/ui/CourseView.java
@@ -30,15 +30,14 @@ public class CourseView extends CustomComponent implements View {
     private String courseID;
     private Button goToMain;
     private Button addStudent;
-    private Button editStudent;
     private Button goToTakeAttendance;
     private Button deleteStudent;
     private Student currStudent;
     private Label courseName;
-    private VerticalLayout popupContent;
     private Grid studentGrid = new Grid();
     private HorizontalLayout topLayout;
     private Label welcome;
+    private Label title;
     private Button logout;
 
     public CourseView() {
@@ -59,14 +58,10 @@ public class CourseView extends CustomComponent implements View {
                 (Button.ClickListener) clickEvent -> goToMain());
         addStudent = new Button("",
                 (Button.ClickListener) clickEvent -> goToStudent());
-        editStudent = new Button("Edit Student",
-                (Button.ClickListener) clickEvent -> editStudent());
         goToTakeAttendance = new Button("Take Attendance For Today",
                 (Button.ClickListener) clickEvent -> takeAttendance());
         deleteStudent = new Button("Delete Student",
                 (Button.ClickListener) clickEvent -> deleteStudent(courseID));
-
-        configurePopup();
         //Display the parameter -- course's student roaster
         studentGrid.setContainerDataSource(new BeanItemContainer<>(Student.class, DatabaseHandler.getInstance().getCourseStudents(courseID)));
         studentGrid.setColumnOrder("id");
@@ -75,7 +70,8 @@ public class CourseView extends CustomComponent implements View {
         editStudentGrid(studentGrid);
         goToTakeAttendance.setId(TAKE_ATTENDANCE_FOR_TODAY_BUTTON_ID);
         logout = new Button("Log Out", (Button.ClickListener) clickEvent -> logOut());
-        welcome = new Label("<h6>"+DatabaseHandler.getInstance().getCourseById(courseID).getCourseName()+"</h6>");
+        welcome = new Label("<center><h2>"+DatabaseHandler.getInstance().getCourseById(courseID).getCourseName()+"</h2></center>");
+        title = new Label();
     }
     
     private void editStudentGrid(Grid studentGrid){
@@ -105,14 +101,6 @@ public class CourseView extends CustomComponent implements View {
         getUI().getNavigator().navigateTo(NewStudentView.NAME);
     }
 
-    private void editStudent() {
-        if (studentGrid.getSelectedRow() != null) {
-            currStudent = (Student) studentGrid.getSelectedRow();
-            editStudentButton(currStudent);
-            popupContent.setVisible(true);
-        }
-    }
-
     private void takeAttendance() {
         getUI().setContent(new AttendanceView(DatabaseHandler.getInstance().getCourseById(courseID)));
     }
@@ -122,37 +110,38 @@ public class CourseView extends CustomComponent implements View {
         welcome.setContentMode(ContentMode.HTML);
         logout.setStyleName("plainbutton");
         goToMain.addStyleName("plainbutton");
-        topLayout = new HorizontalLayout(welcome, goToMain, logout);
+        topLayout = new HorizontalLayout( title, goToMain, logout);
 
         topLayout.setWidth("100%");
-        topLayout.setComponentAlignment(welcome, Alignment.TOP_LEFT);
         topLayout.setComponentAlignment(logout, Alignment.TOP_RIGHT);
         topLayout.setComponentAlignment(goToMain, Alignment.TOP_RIGHT);
         topLayout.addStyleName("topbar");
-        topLayout.setExpandRatio(welcome, 4f);
         topLayout.setExpandRatio(logout, 1f);
         topLayout.setExpandRatio(goToMain, 2f);
         addStudent.setIcon(FontAwesome.PLUS_SQUARE);
         addStudent.addStyleName("tinybutton");
 
-        HorizontalLayout buttons = new HorizontalLayout(editStudent, deleteStudent, goToTakeAttendance);
+        HorizontalLayout buttons = new HorizontalLayout(deleteStudent, goToTakeAttendance);
         buttons.setSpacing(true);
         //buttons.setMargin(new MarginInfo(true, true));
-        VerticalLayout mainLayout = new VerticalLayout(studentGrid,buttons);
+        HorizontalLayout horizontalLayout = new HorizontalLayout(studentGrid, addStudent);
+        VerticalLayout mainLayout = new VerticalLayout(welcome, horizontalLayout ,buttons);
+        
+
         //mainLayout.setSpacing(true);
-        HorizontalLayout horizontalLayout = new HorizontalLayout(topLayout, mainLayout, addStudent,popupContent);
+      
         mainLayout.setSpacing(true);
         //horizontalLayout.setSizeFull();
         //horizontalLayout.setSpacing(true);
         buttons.setComponentAlignment(goToTakeAttendance, Alignment.MIDDLE_CENTER);
-        buttons.setComponentAlignment(editStudent, Alignment.MIDDLE_CENTER);
         buttons.setComponentAlignment(deleteStudent, Alignment.MIDDLE_CENTER);
+        mainLayout.setComponentAlignment(welcome, Alignment.MIDDLE_CENTER);
+        mainLayout.setComponentAlignment(horizontalLayout, Alignment.MIDDLE_CENTER);
         mainLayout.setComponentAlignment(buttons, Alignment.MIDDLE_CENTER);
-        mainLayout.setComponentAlignment(studentGrid, Alignment.MIDDLE_CENTER);
-        horizontalLayout.setComponentAlignment(mainLayout, Alignment.MIDDLE_CENTER);
-        horizontalLayout.setComponentAlignment(popupContent, Alignment.MIDDLE_CENTER);
-        VerticalLayout layout = new VerticalLayout(topLayout, horizontalLayout);
-        layout.setComponentAlignment(horizontalLayout, Alignment.MIDDLE_CENTER);
+        //mainLayout.setComponentAlignment(studentGrid, Alignment.MIDDLE_CENTER);
+       //horizontalLayout.setComponentAlignment(mainLayout, Alignment.MIDDLE_CENTER);
+        VerticalLayout layout = new VerticalLayout(topLayout,mainLayout);
+        //layout.setComponentAlignment(horizontalLayout, Alignment.MIDDLE_CENTER);
         setCompositionRoot(layout);
     }
     private void logOut() {
@@ -162,46 +151,6 @@ public class CourseView extends CustomComponent implements View {
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
-    }
-
-    private void configurePopup() {
-        popupContent = new VerticalLayout();
-        HorizontalLayout popButtons = new HorizontalLayout();
-
-        //TextField id = new TextField("ID");
-        TextField barcode = new TextField("Barcode");
-        barcode.addStyleName("spacer-bot");
-        TextField fname = new TextField("First Name");
-        fname.addStyleName("spacer-bot");
-        TextField lname = new TextField("Last Name");
-        lname.addStyleName("spacer-bot");
-        //popupContent.addComponent(id);
-        popupContent.addComponent(barcode);
-        popupContent.addComponent(fname);
-        popupContent.addComponent(lname);
-
-        Button saveButton = new Button("Save",
-                (Button.ClickListener) clickEvent -> changeStudent( barcode, fname, lname));
-        Button cancelButton = new Button("Cancel",
-                (Button.ClickListener) clickEvent -> popupContent.setVisible(false));
-
-        popButtons.addComponent(saveButton);
-        popButtons.addComponent(cancelButton);
-        popButtons.setSpacing(true);
-
-        popupContent.addComponent(popButtons);
-        popupContent.setVisible(false);
-    }
-
-    private void editStudentButton(Student student) {
-        TextField barcodeEdit = (TextField) popupContent.getComponent(0);
-        TextField firstEdit = (TextField) popupContent.getComponent(1);
-        TextField lastEdit = (TextField) popupContent.getComponent(2);
-
-        barcodeEdit.setValue(student.getBarcode());
-        firstEdit.setValue(student.getFirstName());
-        lastEdit.setValue(student.getLastName());
-
     }
 
     private void deleteStudent(String courseID) {
@@ -215,17 +164,5 @@ public class CourseView extends CustomComponent implements View {
         getUI().getNavigator().addView(CourseView.NAME, new CourseView(courseID));
         getUI().getNavigator().navigateTo(CourseView.NAME);
     }
-
-    private void changeStudent( TextField barcode, TextField firstname, TextField lastname) {
-    	String currID = currStudent.getId();
-    	System.out.println("Current Student ID: "+currID+"\n");
-    	String barcodes = barcode.getValue();
-    	String fname = firstname.getValue();
-    	String lname = lastname.getValue();
-    	DatabaseHandler.getInstance().changeStudent(currID,barcodes,fname,lname, null);
-        popupContent.setVisible(false);
-        Notification saved = new Notification("Saved edit");
-        saved.setDelayMsec(5000);
-        studentGrid.refreshRows(currStudent);
-    }
+    
 }
